b871dc90d012076f7e95571c928096e23c03906a
Rohit Zinzuvadia <rohit.zinzuvadia@gmail.com>
2022-05-14 18:00:38 +0530

    fixed error

--- src/main/java/com/rohit/service/MessageService.java
+++ src/main/java/com/rohit/service/MessageService.java
@@ -7,9 +7,12 @@ import com.rohit.configuration.ApplicationConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
+@Service
 public class MessageService {
     Logger logger = LoggerFactory.getLogger(MessageService.class);
 
